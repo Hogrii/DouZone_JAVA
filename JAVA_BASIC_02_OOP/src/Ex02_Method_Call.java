64bc93b71e8906c5efcc0dfc92dc489ef66bb214
Ljh <hosboy93@naver.com>
2023-03-21 16:31:24 +0900

    폴더 이동

---
+++ JAVA_BASIC_02_OOP/src/Ex02_Method_Call.java
@@ -0,0 +1,16 @@
+import kr.or.kosa.Fclass;
+
+public class Ex02_Method_Call {
+	public static void main(String[] args) {
+		Fclass fclass = new Fclass();                  
+		fclass.m(); // stack
+		
+		fclass.m2(1234);
+		
+		int result = fclass.m3();
+		System.out.println("return value : " + result);
+		
+		result = fclass.m4(10);
+		System.out.println("return value : " + result);
+	}
+}
