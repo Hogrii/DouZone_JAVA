54e6da2f1c5ffa72581e45a0163c5cf5cce2d97c
Ljh <hosboy93@naver.com>
2023-03-23 17:11:12 +0900

    생성자 적용

--- JAVA_BASIC_02_OOP/src/Ex06_Static_AirPlane.java
+++ JAVA_BASIC_02_OOP/src/Ex06_Static_AirPlane.java
@@ -2,6 +2,7 @@ import kr.or.kosa.AirPlane;
 
 public class Ex06_Static_AirPlane {
 	public static void main(String[] args) {
+		/*
 		AirPlane air1 = new AirPlane();
 		air1.setAirPlaneNameAndNum("대한항공", 101);
 		
@@ -10,5 +11,14 @@ public class Ex06_Static_AirPlane {
 		air2.getAirPlaneCnt();
 		
 		// AirPlane.airPlaneCnt -> static도 private이면 접근불가
+		 */
+		
+		AirPlane air1 = new AirPlane(101, "대한항공");
+		air1.getAirPlaneDisplay();
+		air1.getAirPlaneCnt();
+		
+		AirPlane air2 = new AirPlane(102, "아시아나");
+		air2.getAirPlaneDisplay();
+		air2.getAirPlaneCnt();
 	}
 }
--- JAVA_BASIC_02_OOP/src/kr/or/kosa/AirPlane.java
+++ JAVA_BASIC_02_OOP/src/kr/or/kosa/AirPlane.java
@@ -12,6 +12,8 @@ package kr.or.kosa;
  air101.자원 = 대한항공
  air101.자원 = 707
  */
+
+/*
 public class AirPlane {
 	private String airPlaneName;
 	private int airPlaneNum = 0;
@@ -22,6 +24,31 @@ public class AirPlane {
 		this.airPlaneNum = airPlaneNum;
 		airPlaneCnt++;
 		getAirPlaneDisplay();
+	}
+	
+	public void getAirPlaneDisplay() {
+		System.out.println("비행기 번호 : " + airPlaneNum + ", 비행기 이름 : " + airPlaneName);
+	}
+	
+	public void getAirPlaneCnt() {
+		System.out.println("현재까지 만들어진 총 누적대수 : " + airPlaneCnt);
+	}
+}
+*/
+
+// 생성자 이후
+// 생성자를 사용해서 아래 코드를 수정
+public class AirPlane {
+	private int airPlaneNum = 0;
+	private String airPlaneName;	
+	static private int airPlaneCnt = 0;
+	
+	public AirPlane(int num, String name) {
+		airPlaneNum = num;
+		airPlaneName = name;
+		airPlaneCnt++;
+		// 생성자는 함수를 호출 할 수 없다
+//		getAirPlaneDisplay();
 	}
 	
 	public void getAirPlaneDisplay() {
