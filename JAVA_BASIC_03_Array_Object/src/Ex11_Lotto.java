f69c83307685a857bf09ae8769c8d82b78928d3c
LJH <hosboy93@naver.com>
2023-03-29 20:54:36 +0900

    로또 메뉴번호

--- JAVA_BASIC_03_Array_Object/src/Ex11_Lotto.java
+++ JAVA_BASIC_03_Array_Object/src/Ex11_Lotto.java
@@ -4,6 +4,6 @@ public class Ex11_Lotto {
 	public static void main(String[] args) {
 		Lotto lotto = new Lotto();
 		
-		lotto.run();
+		lotto.start();
 	}
 }
--- JAVA_BASIC_03_Array_Object/src/kr/or/kosa/dto/Lotto.java
+++ JAVA_BASIC_03_Array_Object/src/kr/or/kosa/dto/Lotto.java
@@ -1,7 +1,45 @@
 package kr.or.kosa.dto;
 
+import java.util.Scanner;
+
 public class Lotto {
-	public void run(){
-		
+	//아래와 같이 ....
+	private Scanner sc;
+	public Lotto() {
+		sc = new Scanner(System.in);
 	}
+	
+	public void start() {
+		String selectNum = showMenu();
+		if(selectNum.equals("1")) { // 당첨 예상 번호 추출하기
+			
+		}else if(selectNum.equals("2")) { // 프로그램 종료
+			
+		}else {
+			
+		}
+	
+	}
+	
+	
+	private String showMenu() {
+		System.out.println("***************************");
+		System.out.println("**1. 당첨 예상 번호 추출하기**");
+		System.out.println("**2. 프로그램 종료 ^^! ^^! ^^**");
+		System.out.println("***************************");
+		System.out.println("원하는 메뉴 번호를 입력하세요 :");
+		String selectNum = sc.nextLine();
+		return selectNum;
+	}
+	
+	//여러가지 기능을 가지는 함수를 생성 활용하세요
+	// 기능 >> method >> 함수 하나당 기능 하나
+	// public >> 참조변수
+	// private >> 내부 사용 (공통)
+	// 실번호 추출해 주세요
+	// 중복값 나오면 안되요
+	// 낮은 순으로 정렬해 주세요
+	// 화면에 출력해 주세요
+	// 메뉴 기능을 만들어 주세요
+	
 }
