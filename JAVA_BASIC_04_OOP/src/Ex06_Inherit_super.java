05e0d66c65ef1c5c79785f19b4a67f9d137c0f7e
Ljh <hosboy93@naver.com>
2023-03-30 14:59:51 +0900

    super() 호출 순서

--- JAVA_BASIC_04_OOP/src/Ex06_Inherit_super.java
+++ JAVA_BASIC_04_OOP/src/Ex06_Inherit_super.java
@@ -39,7 +39,7 @@ class Derived extends Base {
 	
 	Derived(String dname) {
 		// 부모 생성자 호출(super)
-		super(dname); // 부모 클래스를 메모리에 올릴 때 생성자를 쫓아간다
+//		super(dname); // 부모 클래스를 메모리에 올릴 때 생성자를 쫓아간다
 		
 		this.dname = dname;
 		System.out.println("dname : " + this.dname);
@@ -65,6 +65,12 @@ public class Ex06_Inherit_super {
 		// 부모가 먼저 메모리에 올라가고 자식이 메모리에 올라간다
 		
 		Derived d = new Derived("홍길동");
+		// *********************************************************
+		// 자식 타입인 Derived를 new 했을 때 Derived 안에 super()가 있는지 확인
+		// super()가 없다면 부모의 기본 생성자로 이동
+		// super()가 있다면 부모의 오버로딩 생성자로 이동한다
+		// *********************************************************
+		
 		d.method(); // 재정의가 되면 부모 자원에 접근이 불가
 		
 		// 부모가 그리울땐 super를 사용하면 된다
