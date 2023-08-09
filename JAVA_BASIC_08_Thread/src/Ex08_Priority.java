f66a19e62cc60a3e4ab51b41667396daa5435cdb
Ljh <hosboy93@naver.com>
2023-08-09 14:33:11 +0900

    주석추가

--- JAVA_BASIC_08_Thread/src/Ex08_Priority.java
+++ JAVA_BASIC_08_Thread/src/Ex08_Priority.java
@@ -41,6 +41,8 @@ public class Ex08_Priority {
 		Pth2 pth2 = new Pth2();
 		Pth3 pth3 = new Pth3();
 		
+		// setPriority의 수치에 따라 우선순위가 정해진다
+		// 단, setPriority의 수치가 높다고 무조건 먼저 끝나는 것은 아니다
 		pth.setPriority(10);
 		pth2.setPriority(1);
 		pth3.setPriority(1);
